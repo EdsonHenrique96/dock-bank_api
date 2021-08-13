7dc970d16505edfe8f2bd080074e5f8e5bb60783
Edson Batista <edson.batista@catho.com>
2021-08-13 02:58:56 -0300

    test(deposit): add integration tests

--- tests/integration/account/deposit.spec.ts
+++ tests/integration/account/deposit.spec.ts
@@ -1,3 +1,83 @@
+import { Express } from 'express';
+import { agent as request } from 'supertest';
+
+import { setupApi } from '../../../src/app/api/index';
+
+import {
+  cleanDB,
+  closeConnection,
+  createAccount,
+  createUser,
+  exitingAccountId,
+  nonExistentAccountId,
+  getTransactionById,
+  getAccountById,
+} from '../helpers/db';
+
+const populateDb = async (): Promise<void> => {
+  await createUser();
+  await createAccount();
+};
+
 describe('Deposit', () => {
-  it.todo('todo');
+  let api: Express;
+
+  beforeAll(async () => {
+    api = await setupApi();
+    await populateDb();
+  });
+
+  /**
+   * this should be done to avoid side effects with future testing.
+   */
+  // beforeEach(async () => {
+  //   reset balance
+  //   delete transactions
+  // });
+
+  afterAll(async () => {
+    await cleanDB();
+    await closeConnection();
+  });
+  it('should return 200, create transaction correctly and update balance when the deposit is successful.', async () => {
+    await request(api)
+      .patch(`/account/${exitingAccountId}/balance`)
+      .send({
+        type: 'deposit',
+        amount: 100,
+      })
+      .expect(200)
+      .expect((response) => {
+        const { currentBalance, transactionId } = response.body;
+        expect(currentBalance).toEqual(100);
+        expect(transactionId).toBeTruthy();
+
+        getTransactionById(transactionId)
+          .then((transaction) => {
+            expect(transaction.id).toEqual(transactionId);
+            expect(transaction.amount).toEqual(100);
+            expect(transaction.accountId).toEqual(exitingAccountId);
+            expect(transaction.type).toEqual('deposit');
+          });
+
+        getAccountById(exitingAccountId)
+          .then((account) => {
+            expect(account.id).toBeTruthy();
+            expect(account.balance).toEqual(100);
+          });
+      });
+  });
+  it('should return 422 when account does not exist', async () => {
+    await request(api)
+      .patch(`/account/${nonExistentAccountId}/balance`)
+      .send({
+        type: 'deposit',
+        amount: 100,
+      })
+      .expect(422, {
+        message: 'Account not found.',
+      });
+  });
+  it.todo('should return 412 when the amount to be deposited is equal to or less than 0');
+  it.todo('should not save a transaction to the database when deposit fails');
 });
--- tests/integration/helpers/db.ts
+++ tests/integration/helpers/db.ts
@@ -1,7 +1,8 @@
 import { v4 as uuidV4 } from 'uuid';
 
 import { mysqlClient } from '../../../src/app/infra/modules/mysql-client';
-import { AccountType } from '../../../src/app/models/account';
+import { Account, AccountType } from '../../../src/app/models/account';
+import { Transaction } from '../../../src/app/models/transaction';
 
 export const cleanDB = async (): Promise<void> => {
   await mysqlClient.runQuery({
@@ -34,6 +35,24 @@ export const createAccount = async () : Promise<void> => {
     sqlQuery: 'INSERT INTO account (id, userId, balance, dailyWithdrawalLimit, isActive, accountType) VALUES (?, ?, ?, ?, ?, ?)',
     placeholderValues: [exitingAccountId, existingUserId, 0, 100000, true, AccountType.checking],
   });
+};
+
+export const getTransactionById = async (id: string) : Promise<Transaction> => {
+  const [transaction] = await mysqlClient.runQuery<Transaction[]>({
+    sqlQuery: 'SELECT * FROM transaction WHERE id=? LIMIT 1',
+    placeholderValues: [id],
+  });
+
+  return transaction;
+};
+
+export const getAccountById = async (id: string) : Promise<Account> => {
+  const [account] = await mysqlClient.runQuery<Account[]>({
+    sqlQuery: 'SELECT * FROM account WHERE id=? LIMIT 1',
+    placeholderValues: [id],
+  });
+
+  return account;
 };
 
 export const closeConnection = async (): Promise<void> => {
