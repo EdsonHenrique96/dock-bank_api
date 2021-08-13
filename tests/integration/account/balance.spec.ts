c694b0ba5535a04cf21732e662d91b8ca6bc342e
Edson Batista <edson.batista@catho.com>
2021-08-13 02:03:33 -0300

    test: get balance

--- tests/integration/account/balance.spec.ts
+++ tests/integration/account/balance.spec.ts
@@ -1,3 +1,49 @@
+import { Express } from 'express';
+import { agent as request } from 'supertest';
+
+import { setupApi } from '../../../src/app/api/index';
+
+import {
+  cleanDB,
+  closeConnection,
+  createAccount,
+  createUser,
+  exitingAccountId,
+  nonExistentAccountId,
+} from '../helpers/db';
+
+const populateDb = async (): Promise<void> => {
+  await createUser();
+  await createAccount();
+};
+
 describe('Balance', () => {
-  it.todo('todo');
+  let api: Express;
+
+  beforeAll(async () => {
+    api = await setupApi();
+    await populateDb();
+  });
+
+  afterAll(async () => {
+    await cleanDB();
+    await closeConnection();
+  });
+  it('should return 200 and the balance', async () => {
+    await request(api)
+      .get(`/account/${exitingAccountId}/balance`)
+      .send()
+      .expect(200, {
+        balance: 0,
+      });
+  });
+
+  it('should return 422 when account does not exist', async () => {
+    await request(api)
+      .get(`/account/${nonExistentAccountId}/balance`)
+      .send()
+      .expect(422, {
+        message: 'Account not found.',
+      });
+  });
 });
